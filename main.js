97782615433b944bc2d6891e57c9cc7bf659fbd3
baiysh <baiyshbeksultanov1@gmail.com>
2023-10-12 15:40:55 +0600

    edited head

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Используем "const" для объявления переменных и "addEventListener" для прослушивания событий
 const mainBtn = document.querySelector(".main_button");
 const programmer_button = document.querySelector(".programmer_button");
 const modal = document.querySelector(".modal");
@@ -7,87 +8,72 @@ const modalClose = document.querySelector(".modal_close_icon");
 const defaultForm = document.querySelector(".discount_form");
 const form = document.querySelector(".modal_form");
 const programmerBtn = document.querySelector(".programmer_btn");
+const modalButton = document.querySelector(".modal_button");
 
-const openBtn = () => {
+// Создаем функции для открытия и закрытия модального окна
+const openModal = () => {
   modal.style.display = "flex";
   overlay.style.display = "block";
 };
 
-const closeBtn = () => {
+const closeModal = () => {
   modal.style.display = "none";
   overlay.style.display = "none";
 };
 
-mainBtn.addEventListener("click", openBtn);
-programmerBtn.addEventListener("click", openBtn);
+// Назначаем обработчики событий
+mainBtn.addEventListener("click", openModal);
+programmerBtn.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-modalClose.addEventListener("click", closeBtn);
-overlay.addEventListener("click", closeBtn);
-
+// Используем "const" для объявления URL_APP
 const URL_APP =
   "https://script.google.com/macros/s/AKfycbxTir6LsQhBtLAflQ9A_OFb4W0n6t6M4AIPjcCRMMF_YYsxgiDxTgNDv9_vbcTkyCbLlg/exec";
 
-// находим форму в документе
-
-// указываем адрес отправки формы (нужно только в начале примера)
+// Обновляем атрибут "action" формы
 form.action = URL_APP;
 defaultForm.action = URL_APP;
 
-// вспомогательная функция проверки заполненности формы
-function isFilled(details) {
+// Создаем функцию для проверки заполненности формы
+function isFormFilled(details) {
   const { name, phone } = details;
-  if (!name) return false;
-  if (!phone) return false;
-  return true;
+  return name && phone;
 }
 
-const sendSheet = async ev => {
-  // отменяем действие по умолчанию
+// Создаем функцию для отправки данных
+const sendData = async ev => {
   ev.preventDefault();
-
-  // получаем ссылки на элементы формы
   const name = document.querySelector("[name=name]");
   const phone = document.querySelector("[name=phone]");
-
-  // собираем данные из элементов формы
-  let details = {
+  const details = {
     name: name.value.trim(),
     phone: phone.value.trim(),
   };
 
-  // если поля не заполнены - прекращаем обработку
-  if (!isFilled(details)) return;
+  if (!isFormFilled(details)) return;
 
-  // подготавливаем данные для отправки
-  let formBody = [];
-  for (let property in details) {
-    // кодируем названия и значения параметров
-    let encodedKey = encodeURIComponent(property);
-    let encodedValue = encodeURIComponent(details[property]);
-    formBody.push(encodedKey + "=" + encodedValue);
+  const formBody = new URLSearchParams(details).toString();
+
+  try {
+    const response = await fetch(URL_APP, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      mode: "cors",
+      body: formBody,
+    });
+    await response.json();
+    modalButton.innerText = "Diserahkan";
+  } catch (error) {
+    alert("Ошибка!");
   }
-  // склеиваем параметры в одну строку
-  formBody = formBody.join("&");
 
-  // выполняем отправку данных в Google Apps
-  const result = await fetch(URL_APP, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-    //cors: "no-cors", <- это неправильно
-    mode: "cors", //<- оставим по умолчанию
-    body: formBody,
-  })
-    .then(res => res.json())
-    .catch(err => alert("Ошибка!"));
-  // .then((res) => console.log(res));
-
-  console.log(result);
   name.value = "";
   phone.value = "";
 };
 
-form.addEventListener("submit", sendSheet);
-
-defaultForm.addEventListener("submit", sendSheet);
+// Назначаем обработчики событий для отправки данных
+form.addEventListener("submit", sendData);
+defaultForm.addEventListener("submit", sendData);
